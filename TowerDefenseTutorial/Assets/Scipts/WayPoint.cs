477411c561a9728fce7bd7e03fb6a7da57c891f8
ZerrY <suhan0304@gmail.com>
2023-11-25 18:36:49 +0900

    WayPoint cs Update

--- TowerDefenseTutorial/Assets/Scipts/WayPoint.cs
+++ TowerDefenseTutorial/Assets/Scipts/WayPoint.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WayPoint : MonoBehaviour
+public class WayPoints : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static Transform[] points;
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        points = new Transform[transform.childCount]; //wayPoint 갯수만큼 배열 초기화
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i); //transform의 갯수만큼 자녀를 가져와 points 배열에 할당시켜준다.
+        }
     }
 }
