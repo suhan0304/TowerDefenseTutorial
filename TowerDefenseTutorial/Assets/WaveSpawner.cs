534ede35b98460402645aea0a7824bd981b668a6
ZerrY <suhan0304@gmail.com>
2023-11-26 14:20:31 +0900

    WavecountDown UI Update

--- TowerDefenseTutorial/Assets/WaveSpawner.cs
+++ TowerDefenseTutorial/Assets/WaveSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour
 {
@@ -8,30 +9,36 @@ public class WaveSpawner : MonoBehaviour
 
     public Transform spawnPoint; //몬스터 스폰 위치
 
-    public float timeBetweenWaves = 5f; //웨이브 사이 대기 시간
+    public float timeBetweenWaves = 5.5f; //웨이브 사이 대기 시간
     private float countdown = 2f;
 
-    private int waveNumber = 1;// 웨이브 번호
+    public Text waveCountdownText;
+
+    private int waveIndex = 0;// 웨이브 번호
 
     private void Update()
     {
         if (countdown <= 0f) //카운트다운이 0 보다 작아지먄 Spawn Wave 실행
         {
-            SpawnWave();
+            StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;//카운트 다운을 중간 시간으로 초기화
         }
 
         //deltaTime//마지막 프레임을 그린 후 경과한 시간
         countdown -= Time.deltaTime; //시간을 계속 줄인다.
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString(); //카운트 다운을 반올림한 다음 스트링으로 바꿔서 UI의 텍스트에 설정
     }
 
-    void SpawnWave()
+    IEnumerator SpawnWave() //코루틴
     {
-        for (int i = 0; i < waveNumber; i++) //웨이브 레벨만큼 몬스터 소한
+        waveIndex++; //웨이브가 올때마다 레벨업
+
+        for (int i = 0; i < waveIndex; i++) //웨이브 레벨만큼 몬스터 소한
         {
             SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
         }
-        waveNumber++; //웨이브가 올때마다 레벨업
     }
 
     void SpawnEnemy()
