805399c9654b786a7b86dcd8cb07ed6a2774c599
ZerrY <suhan0304@gmail.com>
2023-11-26 16:26:12 +0900

    Settings Update

--- TowerDefenseTutorial/Assets/Scipts/WayPoint.cs
+++ TowerDefenseTutorial/Assets/Scipts/WayPoint.cs
@@ -8,7 +8,7 @@ public class WayPoints : MonoBehaviour
 
     void Awake()
     {
-        points = new Transform[transform.childCount]; //wayPoint 갯수만큼 배열 초기화
+        points = new Transform[transform.childCount];//wayPoint 갯수만큼 배열 초기화
         for (int i = 0; i < points.Length; i++)
         {
             points[i] = transform.GetChild(i); //transform의 갯수만큼 자녀를 가져와 points 배열에 할당시켜준다.
