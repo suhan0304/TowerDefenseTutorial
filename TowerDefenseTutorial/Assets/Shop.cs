2ca45fd5fa910cbc88d86c107d2bc99d8abb58a3
ZerrY <suhan0304@gmail.com>
2023-12-01 02:39:44 +0900

    Shop cs fix

--- TowerDefenseTutorial/Assets/Shop.cs
+++ TowerDefenseTutorial/Assets/Shop.cs
@@ -16,6 +16,6 @@ public class Shop : MonoBehaviour
     public void PurchaseMissileLauncher() //미사일 런처 건설
     {
         Debug.Log("Missile Launcher Selected"); //테스트용 출력
-        buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab); //다른 터렛을 건설하도록 설정
+        buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab); //미사일 런처를 건설하도록 설정
     }
 }
