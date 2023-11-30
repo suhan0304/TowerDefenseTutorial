102e7e7d180edee948844748bb25e5da25910f07
ZerrY <suhan0304@gmail.com>
2023-11-30 21:49:48 +0900

    Shop cs update

---
+++ TowerDefenseTutorial/Assets/Shop.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void PurchaseStandardTurret() //기본 터렛 건설
+    {
+        Debug.Log("Standard Turret Purchased");
+    }
+    public void PurchaseAnotherTurret() //다른 터렛 건설
+    {
+        Debug.Log("Another Purchased");
+    }
+}
