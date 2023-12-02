8da21a5d0d37248e16a07eb483d1c473e7e8d55c
ZerrY <suhan0304@gmail.com>
2023-12-03 03:15:53 +0900

    Create Shop.cs

---
+++ TowerDefenseTutorial/Assets/Scipts/Shop.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public TurretBlueprint standradTurret; //터렛 청사진
+    public TurretBlueprint MissileLauncher; //미사일 런처 청사진
+    BuildManager buildManager;
+
+    private void Start() //빌드 매니저 참조를 위해 instance 초기화
+    {
+        buildManager = BuildManager.instance;
+    }
+    public void SelectStandardTurret() //기본 터렛 건설
+    {
+        Debug.Log("Standard Turret Selected"); //테스트용 출력
+        buildManager.SelectTurretToBuild(standradTurret); //기본 터렛을 건설하도록 설정
+    }
+    public void SelectMissileLauncher() //미사일 런처 건설
+    {
+        Debug.Log("Missile Launcher Selected"); //테스트용 출력
+        buildManager.SelectTurretToBuild(MissileLauncher); //미사일 런처를 건설하도록 설정
+    }
+}
