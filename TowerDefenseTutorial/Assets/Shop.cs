6fd0f28dfb1335e2df2e5a8983810ec847d4dbb0
ZerrY <suhan0304@gmail.com>
2023-12-03 02:51:10 +0900

    Update Shop cs

--- TowerDefenseTutorial/Assets/Shop.cs
+++ TowerDefenseTutorial/Assets/Shop.cs
@@ -10,14 +10,14 @@ public class Shop : MonoBehaviour
     {
         buildManager = BuildManager.instance;
     }
-    public void PurchaseStandardTurret() //기본 터렛 건설
+    public void SelectStandardTurret() //기본 터렛 건설
     {
         Debug.Log("Standard Turret Selected"); //테스트용 출력
-        buildManager.SetTurretToBuild(buildManager.standardTurretPrefab); //기본 터렛을 건설하도록 설정
+        buildManager.SelectTurretToBuild(standradTurret); //기본 터렛을 건설하도록 설정
     }
-    public void PurchaseMissileLauncher() //미사일 런처 건설
+    public void SelectMissileLauncher() //미사일 런처 건설
     {
         Debug.Log("Missile Launcher Selected"); //테스트용 출력
-        buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab); //미사일 런처를 건설하도록 설정
+        buildManager.SelectTurretToBuild(MissileLauncher); //미사일 런처를 건설하도록 설정
     }
 }
