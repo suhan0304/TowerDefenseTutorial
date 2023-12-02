78c21430605c7623ab3947238b7974b576987787
ZerrY <suhan0304@gmail.com>
2023-12-03 02:49:34 +0900

    Turret Blueprint set

---
+++ TowerDefenseTutorial/Assets/Scipts/TurretBlueprint.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class TurretBlueprint
+{
+    public GameObject prefab; //ÅÍ·¿ ÇÁ¸®ÆÕ
+    public int cost;          //ÅÍ·¿ °Ç¼³ºñ¿ë
+}
