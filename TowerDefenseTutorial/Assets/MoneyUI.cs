c617e05ac754712fcde86e83913a0b258ba5b26b
ZerrY <suhan0304@gmail.com>
2023-12-14 17:23:50 +0900

    Update Money.cs

--- TowerDefenseTutorial/Assets/MoneyUI.cs
+++ TowerDefenseTutorial/Assets/MoneyUI.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MoneyUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text moneyText;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        moneyText.text = "$" + PlayerStats.Money.ToString();
     }
 }
