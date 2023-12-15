adb30ec41944cf5b79a0678ee46beb35ff1398c4
ZerrY <suhan0304@gmail.com>
2023-12-15 20:32:15 +0900

    add player lives

--- TowerDefenseTutorial/Assets/Scipts/PlayerStats.cs
+++ TowerDefenseTutorial/Assets/Scipts/PlayerStats.cs
@@ -7,8 +7,11 @@ public class PlayerStats : MonoBehaviour
     public static int Money; //Static으로 선언하면 메모리 상에 바로 올라가 다른 스크립트에서도 편하게 접근 가능
     public int startMoney = 400; //시작 머니
 
+    public static int Lives; //현재 생명 값
+    public static int startLives = 20; //초기화 값
     void Start()
     {
         Money = startMoney; //게임 시작 시 Money를 시작 머니로 설정    
+        Lives = startLives;
     }
 }
