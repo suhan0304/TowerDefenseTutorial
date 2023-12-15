9dbeb94321230de0310a434f338aed8db4517f83
ZerrY <suhan0304@gmail.com>
2023-12-15 20:56:18 +0900

    update GameManager

--- TowerDefenseTutorial/Assets/GameManager.cs
+++ TowerDefenseTutorial/Assets/GameManager.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{    // Update is called once per frame
+{
+    private bool gameEnded = false; 
+    1
     void Update()
     {
+        if (gameEnded)
+            return;
+
         if(PlayerStats.Lives <= 0) //플레이어 체력이 0 이하
         {
-            EndGame;
+            EndGame(); //게임 종료 함수
         }
+    }
+
+
+    void EndGame()
+    {
+        gameEnded = true;
+        Debug.Log("Game Over!"); //게임 종료
     }
 }
