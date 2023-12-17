dcf76c2513201abd51173092f4feea84fcbb9658
ZerrY <suhan0304@gmail.com>
2023-12-18 04:58:30 +0900

    Fix Speed Error in EnemyMovement.cs

--- TowerDefenseTutorial/Assets/EnemyMovement.cs
+++ TowerDefenseTutorial/Assets/EnemyMovement.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyMovement : MonoBehaviour
 {
     private Transform target; //목표 방향
     private int wavepointIndex = 0;//현재 목표로하는 웨이포인트 인덱스
 
+    private Enemy enemy;
+
     private void Start()
     {
+        enemy = GetComponent<Enemy>();
+
         //WayPoints의 points를 static으로 선언해놨기 때문에 바로 불러올 수 있다.
         //points를 싱글톤 디자인 패턴으로 사용하는 모습
         target = WayPoints.points[0];
@@ -21,7 +26,7 @@ public class EnemyMovement : MonoBehaviour
 
         //방향 벡터로 스피드 만큼 이동
         //방향을 단위벡터로 바꾸기 위해 normalized 진행 후 speed를 곱한만큼 진행 (프레임-시간 보정으로 deltaTime을 사용)
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World); //World Space에서 이동
+        transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World); //World Space에서 이동
 
 
         //웨이포인트 도착 시 다음 웨이포인트로 변경
