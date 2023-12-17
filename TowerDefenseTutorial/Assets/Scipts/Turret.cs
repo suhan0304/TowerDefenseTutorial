79e89baf3bb09620e8c1cd9324f278be56bb527d
ZerrY <suhan0304@gmail.com>
2023-12-18 05:23:35 +0900

    Update Turret.cs

--- TowerDefenseTutorial/Assets/Scipts/Turret.cs
+++ TowerDefenseTutorial/Assets/Scipts/Turret.cs
@@ -135,7 +135,6 @@ public class Turret : MonoBehaviour
         targetEnemy.Slow(slowPct); //둔화 효과 실행
 
         //----- Lase Graphic -----
-
         if (!lineRenderer.enabled)
         {
             lineRenderer.enabled = true; //레이저(라인 렌더러)가 꺼져있으면 키고 나서 위치 설정
