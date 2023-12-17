b7b70a98a8c00986422aba5257c698f948ccc670
ZerrY <suhan0304@gmail.com>
2023-12-18 04:53:31 +0900

    add Movement.cs in Enemy

--- TowerDefenseTutorial/Assets/Scipts/Enemy.cs
+++ TowerDefenseTutorial/Assets/Scipts/Enemy.cs
@@ -6,7 +6,7 @@ public class Enemy : MonoBehaviour
 
     public float health = 100; //몬스터 초기 체력
 
-    public int value = 50; //몬스터를 죽일 시 플레이어에게 주어질 돈
+    public int worth = 50; //몬스터를 죽일 시 플레이어에게 주어질 돈
 
     public GameObject deathEffect;
 
@@ -32,7 +32,7 @@ public class Enemy : MonoBehaviour
 
     void Die()
     {
-        PlayerStats.Money += value; //플레이어에게 돈 지급
+        PlayerStats.Money += worth; //플레이어에게 돈 지급
 
         GameObject effect = (GameObject) Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(effect, 5f);
