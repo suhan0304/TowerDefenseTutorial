99a32a3826d7459810a3a3c80a31012b2d586da4
ZerrY <suhan0304@gmail.com>
2023-12-19 13:33:29 +0900

    Update GameOver.cs

--- TowerDefenseTutorial/Assets/GameOver.cs
+++ TowerDefenseTutorial/Assets/GameOver.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
@@ -10,5 +11,16 @@ public class GameOver : MonoBehaviour
     void OnEnable() //게임 오버 UI가 Active 시 실행
     {
         roundsText.text = PlayerStats.Rounds.ToString(); //라운드 설정
+    }
+
+    public void Retry()
+    {
+        //이름 또는 인덱스로 로드 씬 가능
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //현재 로드 중인 MainScene을 그대로 다시 불러옴
+    }
+
+    public void Menu()
+    {
+        Debug.Log("Go to menu.") // Menu ui 구현 후 연결
     }
 }
