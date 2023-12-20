d9bbed41ea61ab11406f122a83d2ccff27e6ea63
ZerrY <suhan0304@gmail.com>
2023-12-20 16:53:04 +0900

    Update CS

--- TowerDefenseTutorial/Assets/NodeUI.cs
+++ TowerDefenseTutorial/Assets/NodeUI.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class NodeUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Node target;
 
-    // Update is called once per frame
-    void Update()
+    public void SetTarget(Node _target)
     {
-        
+        target = _target;
+
+        transform.position = target.GetBuildPosition(); // 노드 포지션이 아니라 빌드 포지션을 가져온다
+                                                        // 노드 포지션이면 노드의 정중앙을 기준으로 위치
+
     }
 }
--- TowerDefenseTutorial/Assets/Scipts/BuildManager.cs
+++ TowerDefenseTutorial/Assets/Scipts/BuildManager.cs
@@ -21,7 +21,9 @@ public class BuildManager : MonoBehaviour
 
     private TurretBlueprint turretToBuild; //노드 선택 시 건설할 터렛\
     private Node selectNode; // 선택한 노드
-    
+
+    public NodeUI nodeUI;
+
     public bool CanBuild { get { return turretToBuild != null; } } // 터렛을 건설할 수 있는지 확인하는 부울 변수 ( Build할 Turret이 Null이 아니면 True 반환 )
     public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } } // 터렛 비용보다 소지한 돈이 많은지 확인하는 함수
 
@@ -47,6 +49,8 @@ public class BuildManager : MonoBehaviour
     {
         selectNode = node; //선택한 노드에 매개변수 노드를 연결
         turretToBuild = null; //건물 건설은 더 이상 진행 X
+
+        nodeUI.SetTarget(node);
     }
 
     public void SelectTurretToBuild (TurretBlueprint turret)
