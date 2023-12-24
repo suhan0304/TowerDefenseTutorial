6956ad863ce50d53260cc06028be7c417c3d8bfe
ZerrY <suhan0304@gmail.com>
2023-12-25 05:25:58 +0900

    Create MainMenu.cs

---
+++ TowerDefenseTutorial/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad = "MainLevel";
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad); //MainLevel 이름의 씬을 로드
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting..."); //유니티 에디터에선 실제로 종료 확인이 안 됨 : 출력으로 확인
+        Application.Quit(); 
+    }
+}
