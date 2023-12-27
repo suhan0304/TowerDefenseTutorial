cbde7a5c6d560a152efaf006202c58c5e266acf0
ZerrY <suhan0304@gmail.com>
2023-12-28 03:58:58 +0900

    update SceneFader

--- TowerDefenseTutorial/Assets/SceneFader.cs
+++ TowerDefenseTutorial/Assets/SceneFader.cs
@@ -7,7 +7,12 @@ using UnityEngine.SceneManagement;
 public class SceneFader : MonoBehaviour
 {
     public Image img;
+    public AnimationCurve curve;
 
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
     IEnumerator FadeIn ()
     {
         float t = 1f;
@@ -15,6 +20,8 @@ public class SceneFader : MonoBehaviour
         while (t > 0f)
         {
             t -= Time.deltaTime; //t를 프레임 단위 시간만큼 감소 
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a); //투명도를 a로 설정 (투명도가 animationCurve를 따라 감소)
             yield return 0; //다음 프레임으로 넘어감
         }
     }
