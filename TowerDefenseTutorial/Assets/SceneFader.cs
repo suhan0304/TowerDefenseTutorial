5f1af7b1fc84266272f76e4e8b11b735eb577b8e
ZerrY <suhan0304@gmail.com>
2023-12-28 03:52:09 +0900

    Update SceneFader.cs

--- TowerDefenseTutorial/Assets/SceneFader.cs
+++ TowerDefenseTutorial/Assets/SceneFader.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class SceneFader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Image img;
 }
