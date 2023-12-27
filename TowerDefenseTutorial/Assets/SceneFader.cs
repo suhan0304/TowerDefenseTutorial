d69bfb1f7af915348e80b91dd15060778f040adc
ZerrY <suhan0304@gmail.com>
2023-12-28 03:54:41 +0900

    Update SceneFader.cs

--- TowerDefenseTutorial/Assets/SceneFader.cs
+++ TowerDefenseTutorial/Assets/SceneFader.cs
@@ -7,4 +7,15 @@ using UnityEngine.SceneManagement;
 public class SceneFader : MonoBehaviour
 {
     public Image img;
+
+    IEnumerator FadeIn ()
+    {
+        float t = 1f;
+
+        while (t > 0f)
+        {
+            t -= Time.deltaTime; //t를 프레임 단위 시간만큼 감소 
+            yield return 0; //다음 프레임으로 넘어감
+        }
+    }
 }
