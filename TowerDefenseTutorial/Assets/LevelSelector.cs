5357d60c7a2604520000597af14880eae22facbd
ZerrY <suhan0304@gmail.com>
2023-12-31 01:54:40 +0900

    Update Button Animation

--- TowerDefenseTutorial/Assets/LevelSelector.cs
+++ TowerDefenseTutorial/Assets/LevelSelector.cs
@@ -7,6 +7,13 @@ public class LevelSelector : MonoBehaviour
 
     public Button[] levelButtons;
 
+    public void Start()
+    {
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            levelButtons[i].interactable = false; //시작하면 일단 모든 버튼은 상호작용 불가 (클릭안됨)
+        }
+    }
 
     public void Select(string levelName)
     {
