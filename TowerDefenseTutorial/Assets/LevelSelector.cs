c0cba0071cb244b83cb59f1ce295b26bf747d5e9
ZerrY <suhan0304@gmail.com>
2023-12-31 01:49:13 +0900

    Update LevelSelector.cs

--- TowerDefenseTutorial/Assets/LevelSelector.cs
+++ TowerDefenseTutorial/Assets/LevelSelector.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
     public SceneFader fader;
+
+    public Button[] levelButtons;
+
+
     public void Select(string levelName)
     {
         fader.FadeTo(levelName); //선택한 레벨 장면으로 Fade 효과 주면서 이동
